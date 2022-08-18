fc73883a8c57430c26505a97d46386a490998a64
Asinus Grandus <91527907+AsinusGrandus@users.noreply.github.com>
2022-08-18 19:51:26 +0200

    Added 'Block' class

--- server.js
+++ server.js
@@ -1,73 +1,93 @@
 //  npm run devStart
 const fs = require('fs');
 const express = require('express');
-const app = express();
+const { type } = require('os');
 
-function getBlocklist(){
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
+class Block {
+    constructor(id, longName, hexadecimal, bits) {
+        this.id = id;
+        this.hexadecimal = hexadecimal;
+
+        this.bits = bits.substring(2, 15);
+        this.firstFourBits = this.bits.substring(0, 4)
+        this.lastNineBits = this.bits.substring(4)
+
+        this.longName = longName;
+        this.name = longName == "null" ? "null": longName.split("[")[0].substring(10) ; // split long name into minecraft:name and details]
+        this.details = longName.includes("[") ? longName.split("[")[1].slice(0, -1).split(",") : "no details"; // split long name into minecraft:name and details], remove ] at the end and split into list
+    }
+}
+
+function getBlocks(){
     let blocklist = fs.readFileSync('data/block_state_id_list_by_masa.txt', 'utf8');
     let data = new Object();
+    // create Block instance for each line
     blocklist.split(/\r?\n/).forEach(line =>  {
         const blockData = line.split(" ");
-        data[blockData[0]] = {
-            "id": blockData[0],
-            "name": blockData[3],
-            "0x": blockData[1],
-            "bits": blockData[2].substring(2, 15),
-        }
+        data[blockData[0]] = new Block(blockData[0], blockData[3], blockData[1], blockData[2])
     })
+    // object containing Block instances
     return data
 }
 
 function getIdbyName(block_name) {
     let id = new String();
-    const data = getBlocklist()
+    const data = getBlocks()
+
     for (let element of Object.keys(data)) {
-        const name = data[element]["name"]
-        const shortname = name.split("[")[0].substring(10)
-        if (block_name == name || block_name == shortname) {
-            id = element
+        const name = data[element].name;
+        if (block_name == name) {
+            id = data[element].id;
             break
         }
     }
     return id
 }
 
-app.use(express.urlencoded({ extended: true }))
-
-app.set('view engine', 'ejs')
-
 app.get("/", (req, res) => {
     res.render("home")
 })
 
 app.get("/api/blocklist", (req, res) => {
-    const data = getBlocklist();
+    const data = getBlocks();
     res.send(data);
 })
 
 app.post("/submit", async (req, res) => {
-    const data = getBlocklist();
-    const name = req.body.block_name;
-    const id = await getIdbyName(name)
+    const data = getBlocks();
+    const idName = req.body.block_name;
 
-    const blockInfo = data[id];
-    const firstBits = blockInfo["bits"].substring(0, 4);
-    const lastBits = blockInfo["bits"].substring(4);
+    if (idName == "") {
+        res.redirect("/")
+        return
+    }
+
+    const name = idName.split(" ")[1] // split id: name into ['id:', 'name']
+    const submittedBlock = data[await getIdbyName(name)];
 
     let firstBlocks = new Array();
     let lastBlocks = new Array();
 
     Object.keys(data).forEach((element) => {
         const block = data[element];
-        if (block["name"] == name.toString() || block["name"] == "null") { return }
-        if (block["bits"].substring(0, 4) === firstBits) {
-            firstBlocks.push(block["name"])
-        } else if (block["bits"].substring(4) === lastBits) {
-            lastBlocks.push(block["name"])
+        
+        if (block.name == name.toString() || block.name == "null") { return }
+        
+        // It is a valid block and not itself
+        if (block.firstFourBits === submittedBlock.firstFourBits) {
+            // First 4 bits from the block match
+            firstBlocks.push(block)
+        } else if (block.lastNineBits === submittedBlock.lastNineBits) {
+            // Last 9 bits from the block match
+            lastBlocks.push(block)
         } else { return }
     })
 
-    res.render("result", { block_name: name, first_blocks: firstBlocks, last_blocks: lastBlocks})
+    res.render("result", { block_name: name, first_blocks: JSON.stringify(firstBlocks), last_blocks: JSON.stringify(lastBlocks)})
 })
 
 app.listen(process.env.PORT || 3000)
