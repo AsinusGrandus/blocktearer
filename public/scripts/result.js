0458781ed0a3ebb144af4c978394e8dfc16675b2
Asinus Grandus <91527907+AsinusGrandus@users.noreply.github.com>
2022-08-21 20:13:17 +0200

    Tabs get added dynamically

--- public/scripts/result.js
+++ public/scripts/result.js
@@ -1,20 +1,89 @@
+// The colours from the wool combined with the number of bits that get cut of at that location
+const woolData = {
+    "white": {'bits': 0, 'colour': '#f2f5f3'},
+    "orange": {'bits': 1, 'colour': '#ffcc75'},
+    "magenta": {'bits': 2, 'colour': '#ff75fa'},
+    "light-blue": {'bits': 3, 'colour': '#75fffd'},
+    "yelllow": {'bits': 4, 'colour': '#fffd75'},
+    "lime": {'bits': 5, 'colour': '#75ff7a'},
+    "pink": {'bits': 6, 'colour': '#ff75a8'},
+    "gray": {'bits': 7, 'colour': '#757575'},
+    "light-gray": {'bits': 8, 'colour': '#a19f9f'},
+    "cyan": {'bits': 9, 'colour': '#75ffc8'},
+    "purple": {'bits': 10, 'colour': '#df75ff'},
+    "blue": {'bits': 11, 'colour': '#7577ff'},
+    "brown": {'bits': 12, 'colour': '#918474'},
+};
 
+// Get the tabnav <ul>
+const tabnav = document.getElementById('tabnav');
+// Get the tabcontent <div>
+const tabcontent = document.getElementById('tabcontent');
 
-// Tab handling
+Object.keys(woolData).forEach((wooltype) => {
+    // Create a new tab <li> that looks like: <li class='tab active ' data-tab-target="#white">white</li>
+    const tab = document.createElement('li');
+
+    // Create a new colour <div> that looks like: <div id="orange" data-tab-content></div>
+    const colour = document.createElement('div');
+    const p = document.createElement('p');
+
+    if (wooltype == 'white') { 
+        tab.classList.add('active'); 
+        colour.classList.add('active');
+    }
+
+    // Give the tab <li> properties
+    tab.innerHTML = wooltype;
+    tab.id = "#" + wooltype;
+    tab.classList.add('tab');
+    tab.dataset.tabTarget = "#" + wooltype;
+    tab.style.color = woolData[wooltype]['colour'];
+    tab.dataset.tabColour = woolData[wooltype]['colour'];
+
+    // Give the colour <div> properties
+    colour.id = wooltype;
+    colour.dataset.tabContent = '';
+
+    // Give the p <p> properties and add to colour <div>
+    p.innerHTML = `The wordtearing needs to take place in the location from the ${wooltype} wool block`;
+    colour.appendChild(p)
+
+    // Add tab <li> to tabnav <ul>
+    tabnav.appendChild(tab);
+    // Add colour <div> to tabcontent <div>
+    tabcontent.appendChild(colour);
+})
+
+// Get all tabs with the tabTarget property
 const tabs = document.querySelectorAll('[data-tab-target]');
-const tabContents = document.querySelectorAll('[data-tab-content]')
+// Get all tab contents with the tabContent property
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
 tabs.forEach((tab) => {
+    // Add an event listener to the tab <li>
     tab.addEventListener('click', () => {
+        // tab.dataset.tabTarget is the id from the tab content <div>
         const target = document.querySelector(tab.dataset.tabTarget)
+
+        // Remove active class from all tabContents <div> and tabs <li>
         tabContents.forEach((content) => {
             content.classList.remove('active')
         })
         tabs.forEach((tab) => {
             tab.classList.remove('active')
+            // Remove background color and set color
+            tab.style.color = tab.dataset.tabColour
+            tab.style.backgroundColor = '';
         })
 
+        // Make tab active
         tab.classList.add('active');
         target.classList.add('active');
+
+        // Make the text black and set the backgroundcolor
+        tab.style.color = 'black';
+        tab.style.backgroundColor = tab.dataset.tabColour;
     })
 })
 
