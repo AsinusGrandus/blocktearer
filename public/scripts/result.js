a6a3e373ee55075b4a68e5b4caabf68b1ce86d61
Asinus Grandus <91527907+AsinusGrandus@users.noreply.github.com>
2022-08-21 18:38:28 +0200

    Tabs

--- public/scripts/result.js
+++ public/scripts/result.js
@@ -1,3 +1,26 @@
+
+
+// Tab handling
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]')
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach((content) => {
+            content.classList.remove('active')
+        })
+        tabs.forEach((tab) => {
+            tab.classList.remove('active')
+        })
+
+        tab.classList.add('active');
+        target.classList.add('active');
+    })
+})
+
+
+
+// Other stuff
 const columnIds = ['#first_blocks_column_grid', '#last_blocks_column_grid']
 
 // Create two columns with blocks
